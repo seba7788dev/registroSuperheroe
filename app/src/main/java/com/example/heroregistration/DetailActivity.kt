d67810595e4a17cd409af09fdee8f4cc4b6ccc28
unknown <seby77_88@hotmail.com>
2022-08-04 00:16:37 -0300

    intent y detail activity

--- app/src/main/java/com/example/heroregistration/DetailActivity.kt
+++ app/src/main/java/com/example/heroregistration/DetailActivity.kt
@@ -2,10 +2,27 @@ package com.example.heroregistration
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.heroregistration.databinding.ActivityDetailBinding
+import com.example.heroregistration.databinding.ActivityMainBinding
 
 class DetailActivity : AppCompatActivity() {
+    lateinit var binding: ActivityDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding= ActivityDetailBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        setContentView(binding.root)
+
+        val bundle= intent.extras!!
+        val superhero_name=bundle.getString("superhero_name") ?: ""
+        val alterego_hero=bundle.getString("alterego_hero") ?: ""
+        val biografia_hero=bundle.getString("biografia_hero") ?: ""
+        val power_hero=bundle.getFloat("power_hero")
+
+        binding.heroName.text=superhero_name
+        binding.alterEgotv.text=alterego_hero
+        binding.biografiaTv.text=biografia_hero
+        binding.powerHero.rating=power_hero
+
+
     }
 }
--- app/src/main/java/com/example/heroregistration/MainActivity.kt
+++ app/src/main/java/com/example/heroregistration/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.heroregistration
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.heroregistration.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding= ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.saveBoton.setOnClickListener {
+            val superHeroName= binding.heroNameEdit.text.toString()
+            val alterEgo=binding.alteregoEdit.text.toString()
+            val biografia= binding.biografiaEditText.text.toString()
+            val power=binding.ratingBar.rating
+            openDetailActivity(superHeroName,alterEgo,biografia,power)
+
+        }
+    }
+
+    private fun openDetailActivity(name:String, alter:String, bio:String, power:Float) {
+        val intent= Intent(this, DetailActivity::class.java)
+        intent.putExtra("superhero_name", name)
+        intent.putExtra("alterego_hero",alter)
+        intent.putExtra("biografia_hero",bio)
+        intent.putExtra("power_hero",power)
+        startActivity(intent)
     }
 }
