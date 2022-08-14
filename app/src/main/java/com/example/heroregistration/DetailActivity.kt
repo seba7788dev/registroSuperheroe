70bab070ab517e0769e11b49530246b13c70d6c1
seba7788dev <seby77_88@hotmail.com>
2022-08-14 19:22:07 -0300

    camara e imagen se pasa a la sig activity

--- app/src/main/java/com/example/heroregistration/DetailActivity.kt
+++ app/src/main/java/com/example/heroregistration/DetailActivity.kt
@@ -1,5 +1,6 @@
 package com.example.heroregistration
 
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.heroregistration.databinding.ActivityDetailBinding
@@ -8,6 +9,7 @@ import com.example.heroregistration.databinding.ActivityMainBinding
 class DetailActivity : AppCompatActivity() {
     companion object{
         const val SUPERHERO_KEY = "superhero"
+        const val BITMAP_KEY="bitmap"
 
     }
     lateinit var binding: ActivityDetailBinding
@@ -18,6 +20,11 @@ class DetailActivity : AppCompatActivity() {
 
         val bundle= intent.extras!!
         val superhero=bundle.getParcelable<Superhero>(SUPERHERO_KEY)
+        val bitmap=intent.extras!!
+        val image=bitmap.getParcelable<Bitmap>(BITMAP_KEY)
+
+        binding.imageView.setImageBitmap(image)
+
 /*
         binding.heroName.text=superhero?.nombre
         binding.alterEgotv.text=superhero?.alterego
@@ -30,3 +37,40 @@ class DetailActivity : AppCompatActivity() {
 
     }
 }
+//************************************************************
+//************************************************************
+// OTRA FORMA
+/**
+class DetailActivity : AppCompatActivity() {
+companion object{
+const val SUPERHERO_KEY = "superhero"
+const val BITMAP_KEY="bitmap"
+
+}
+lateinit var binding: ActivityDetailBinding
+override fun onCreate(savedInstanceState: Bundle?) {
+binding= ActivityDetailBinding.inflate(layoutInflater)
+super.onCreate(savedInstanceState)
+setContentView(binding.root)
+
+val bundle= intent.extras!!
+val superhero=bundle.getParcelable<Superhero>(SUPERHERO_KEY)
+val bitmap=intent.extras!!
+val image=bitmap.getParcelable<Bitmap>(BITMAP_KEY)
+
+binding.imageView.setImageBitmap(image)
+
+/*
+binding.heroName.text=superhero?.nombre
+binding.alterEgotv.text=superhero?.alterego
+binding.biografiaTv.text=superhero?.bio
+binding.powerHero.rating=superhero?.rating ?: 0.0f*/
+
+//el superhero viene del xml, declarada la variable del data binding
+binding.superhero = superhero
+
+
+}
+}
+
+ * */
--- app/src/main/java/com/example/heroregistration/MainActivity.kt
+++ app/src/main/java/com/example/heroregistration/MainActivity.kt
@@ -10,12 +10,20 @@ import android.provider.MediaStore
 import android.text.PrecomputedText
 import android.widget.ImageView
 import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.UnusedAppRestrictionsConstants
 import com.example.heroregistration.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var heroImage:ImageView
+    private lateinit var guardarImagen:Bitmap
+    private val getContent=registerForActivityResult(ActivityResultContracts.TakePicturePreview()){ bitmap ->
+        guardarImagen=bitmap
+        heroImage.setImageBitmap(guardarImagen)
+
+
+    }
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         binding= ActivityMainBinding.inflate(layoutInflater)
@@ -40,37 +48,24 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun openCamera() {
-        // implicit intent, para abrir la camara
-        startForResult.launch(Intent(MediaStore.ACTION_IMAGE_CAPTURE))
 
-    }
-    val startForResult= registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-        result: ActivityResult ->
-        if (result.resultCode == Activity.RESULT_OK){
-            val intent = result.data
-            val imageBitmap= intent?.extras?.get("data") as Bitmap
-            heroImage.setImageBitmap(imageBitmap)
-        }
+        getContent.launch(null)
+
+
+
     }
 
     private fun openDetailActivity(superhero:Superhero) {
         //explicit intent != implicit intent
         val intent= Intent(this, DetailActivity::class.java)
         intent.putExtra(DetailActivity.SUPERHERO_KEY, superhero)
+        intent.putExtra(DetailActivity.BITMAP_KEY,guardarImagen)
         startActivity(intent)
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
 
-        if (requestCode == 1000 && resultCode == Activity.RESULT_OK){
-            //guardo la foto capturada
-            val extras=data?.extras
-            val heroBitmap=extras?.getParcelable<Bitmap>("data")
 
-            heroImage.setImageBitmap(heroBitmap)
-        }
-    }
+
 }
 /*
 *  private fun openCamera() {
@@ -97,3 +92,69 @@ class MainActivity : AppCompatActivity() {
             heroImage.setImageBitmap(heroBitmap)
         }
     }*/
+
+//***********************************************************************
+//***********************************************************************
+// OTRA FORMA
+/**
+
+class MainActivity : AppCompatActivity() {
+private lateinit var heroImage:ImageView
+private lateinit var guardarImagen:Bitmap
+lateinit var binding: ActivityMainBinding
+override fun onCreate(savedInstanceState: Bundle?) {
+binding= ActivityMainBinding.inflate(layoutInflater)
+super.onCreate(savedInstanceState)
+setContentView(binding.root)
+
+binding.saveBoton.setOnClickListener {
+val superHeroName= binding.heroNameEdit.text.toString()
+val alterEgo=binding.alteregoEdit.text.toString()
+val biografia= binding.biografiaEditText.text.toString()
+val power=binding.ratingBar.rating
+
+val hero= Superhero(superHeroName,alterEgo,biografia,power)
+openDetailActivity(hero)
+
+
+}
+heroImage=binding.heroImage
+heroImage.setOnClickListener {
+openCamera()
+}
+}
+
+private fun openCamera() {
+val intent =Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+getResult.launch(intent)
+
+
+
+}
+private val getResult=registerForActivityResult(
+ActivityResultContracts.StartActivityForResult()) {
+if(it.resultCode == Activity.RESULT_OK){
+//val value = it.data?.getStringExtra("input")
+val resultado=it.data
+
+val picture= resultado?.extras?.get("data") as Bitmap
+guardarImagen=picture
+
+
+binding.heroImage.setImageBitmap(guardarImagen)
+}
+}
+
+
+private fun openDetailActivity(superhero:Superhero) {
+//explicit intent != implicit intent
+val intent= Intent(this, DetailActivity::class.java)
+intent.putExtra(DetailActivity.SUPERHERO_KEY, superhero)
+intent.putExtra(DetailActivity.BITMAP_KEY,guardarImagen)
+startActivity(intent)
+}
+
+
+}
+
+ * */
